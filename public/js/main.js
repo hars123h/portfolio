7b36c93c67bf9aaf54377085bf7dd79be8412f01
unknown <tripathihk2014@gmail.com>
2020-09-12 14:37:35 +0530

    Initial Commit

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+
+//   let $btns = $(".project-area .button-group button");
+
+//   $btns.click(function(e){
+//     $(".project-area .button-group button").removeClass("active");
+//     e.target.classList.add("active");
+//     let selector = $(e.target).attr("data-filter");
+//     $(".project-area .grid").isotope({
+//       filter:selector
+//     });
+//     return false;
+//   })
+
+//   $(".project-area .button-group #btn1").trigger("click");
+
+//   $('.project-area .grid .test-popup-link').magnificPopup({
+//     type: 'image',
+//     // other options
+//     gallery:{enabled:true}
+//   });
+
+// ********************Navbar Collapsed *****************************
+
+        $('.navbar-collapse a').click(function(){
+            $(".navbar-collapse").collapse('hide');
+        });
+
+  // owl-carousel
+  $('.about-area .owl-carousel').owlCarousel({
+    loop:true,
+    autoplay:true,
+    dots:true,
+    responsive:{
+      0:{
+        items:1
+      },
+      544:{
+        items:2
+      }
+    }
+
+  });
+  // Sticky navigaton bar menu
+
+  let nav_offset_top = $(".header_area").height()+50;
+
+  function navbarFixed(){
+    if($(".header_area").length){
+      $(window).scroll(function(){
+        let scroll = $(window).scrollTop();
+        if(scroll >= nav_offset_top){
+          $(".header_area").addClass("navbar_fixed");
+        }else{
+          $(".header_area").removeClass("navbar_fixed");
+
+        }
+      })
+    }
+  }
+  navbarFixed();
+
+
+
+});
